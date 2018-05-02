24ffb720ff5e1a2148bfc785cacf8b5c7caf6112
ameibj <576164240@qq.com>
2018-05-02 15:38:14 +0800

    添加2028文件

---
+++ 2048.js
@@ -0,0 +1,308 @@
+//  面向对象
+var game = {
+    data: [
+        // [2, 4, 2, 4],
+        // [4, 2, 4, 2],
+        // [2, 4, 2, 4],
+        // [4, 2, 4, 2]
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    PLAYING: 1, // 游戏进行状态
+    GAME_OVER: 0, // 游戏结束状态
+    state: 1,
+    score: 0, // 保存游戏分数
+    /*判断数组是否满*/
+    isFull: function () {
+        /*判断当前数组是否不为0*/
+        /*只要有任意一个===0 返回false*/
+        for (var row = 0; row < this.data.length; row++) {
+            for (var col = 0; col < this.data[row].length; col++) {
+                if (this.data[row][col] === 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    },
+    randomNum: function () {
+        if (!this.isFull()) {
+            while (true) {
+                // 向data中随机数生成 2-4
+                var row = parseInt(Math.random() * 4);
+                var col = parseInt(Math.random() * 4);
+                // 随机生成2或4
+                var n = Math.random() < 0.5 ? 2 : 4;
+                // 判断当前单元格是否有数字
+                if (this.data[row][col] === 0) {
+                    this.data[row][col] = n;
+                    break;
+                }
+            }
+        }
+    },
+    start: function () {
+        this.randomNum();
+        this.randomNum();
+        // 将数组完整显示到div
+        this.showView();
+    },
+    // 将数组完整显示到div
+    showView: function () {
+        for (var row = 0; row < this.data.length; row++) {
+            for (var col = 0; col < this.data[row].length; col++) {
+                var odiv = document.querySelector('#fc' + row + col);
+                var n = this.data[row][col]
+                odiv.innerHTML = n === 0 ? '' : n;
+                odiv.className = n === 0 ? 'fcell' : 'fcell n' + n;
+            }
+        }
+        var score = document.querySelector('.score').querySelector("span");
+        score.innerText = this.score;
+    },
+    /*封装动画和创建随机数，数据刷新*/
+    anmaiteStart :function(){
+        // 执行动画
+        animation.start();
+        setTimeout(function () {
+            //  生成一个随机数，并添加到数组展示
+            //    console.log(this === game)  // true bind(this)用法
+            this.randomNum();
+            this.showView();
+
+        }.bind(this), animation.interval * animation.times);
+    },
+    /*向左*/
+    moveLeft: function () {
+        if (this.canLeft()) {
+            for (var row = 0; row < this.data.length; row++) {
+                this.moveLeftInRow(row);
+            }
+            this.anmaiteStart(); // 执行动画和刷新页面数据
+
+        }
+    },
+    /* 任意一行内移动或合并*/
+    moveLeftInRow: function (row) {
+        var cells = this.data[row];
+        for (var i = 0; i < cells.length - 1; i++) {
+            //  从当前行位置向后找
+            var nexti = this.getLeftNext(row, i);
+            if (nexti != -1) {
+                if (cells[i] === 0) {
+                    cells[i] = cells[nexti];
+                    animation.addTask('' + row + nexti, '' + row + i);// 动画
+                    cells[nexti] = 0;
+                    i--;
+                } else if (cells[i] === cells[nexti]) {
+                    cells[i] += cells[nexti];
+                    animation.addTask('' + row + nexti, '' + row + i);// 动画
+                    cells[nexti] = 0;
+                    this.score += cells[i]; // 得分
+                }
+            }
+        }
+    },
+    // 从当前位置向右找不为0 的数
+    getLeftNext: function (row, start) {
+        var cells = this.data[row];
+        for (var i = start + 1; i < 4; i++) {
+            if (cells[i] !== 0) {
+                return i;
+            }
+        }
+        return -1;
+    },
+
+    /*向右*/
+    moveRight: function () {
+        if (this.canRight()) {
+            for (var row = 0; row < this.data.length; row++) {
+                this.moveRightInRow(row);
+            }
+            // 生成一个随机数，并添加到数组展示
+            this.anmaiteStart(); // 执行动画和刷新页面数据
+        }
+    },
+    moveRightInRow: function (row) {
+        // 从右向左遍历 到>0 结束
+        for (var i = this.data[row].length - 1; i > 0; i--) {
+            // 找当前行的开始位置的下标
+            var previ = this.getRightPrev(row, i);
+            if (previ !== -1) {
+                if (this.data[row][i] === 0) {
+                    this.data[row][i] = this.data[row][previ];
+                    animation.addTask('' + row + previ, '' + row + i);// 动画
+                    this.data[row][previ] = 0;
+                    i++;
+                } else if (this.data[row][previ] === this.data[row][i]) {
+                    this.data[row][i] += this.data[row][previ];
+                    animation.addTask('' + row + previ, '' + row + i);// 动画
+                    this.data[row][previ] = 0;
+                    this.score += this.data[row][i];
+                }
+            } else {
+                break;
+            }
+        }
+    },
+    // 向左找第一个不为0的数
+    getRightPrev: function (row, start) {
+        var cells = this.data[row];
+        for (var i = start - 1; i >= 0; i--) {
+            if (cells[i] !== 0) {
+                return i;
+            }
+        }
+        return -1;
+    },
+
+    /*向下*/
+    moveDown: function () {
+        if (this.canDown()) {
+            for (var col = 0; col < 4; col++) {
+                this.moveDownInCol(col);
+            }
+            this.anmaiteStart(); // 执行动画和刷新页面数据
+        }
+    },
+    moveDownInCol: function (col) { // 计算合并逻辑
+        for (var row = this.data.length - 1; row > 0; row--) {
+            var prevRow = this.getDownNext(row, col);
+            if (prevRow !== -1) {
+                if (this.data[row][col] === 0) {
+                    this.data[row][col] = this.data[prevRow][col];
+                    animation.addTask('' + prevRow + col, '' + row + col);// 动画
+                    this.data[prevRow][col] = 0;
+                    row++;
+                } else if (this.data[prevRow][col] === this.data[row][col]) {
+                    this.data[row][col] += this.data[prevRow][col];
+                    animation.addTask('' + prevRow + col, '' + row + col);// 动画
+                    this.data[prevRow][col] = 0;
+
+                    this.score += this.data[row][col];
+                }
+            } else {
+                break;
+            }
+        }
+    },
+    getDownNext: function (startRow, col) {
+        for (var i = startRow - 1; i >= 0; i--) {
+            if (this.data[i][col] !== 0) {
+                return i;
+            }
+        }
+        return -1;
+    },
+    /*向上*/
+    moveUp: function () {
+        if (this.canUp()) {
+            for (var col = 0; col < 4; col++) {
+                this.moveUpInCol(col);
+            }
+            this.anmaiteStart(); // 执行动画和刷新页面数据
+        }
+    },
+    moveUpInCol: function (col) {
+        for (var row = 0; row < 4; row++) {
+            var nextRowIndex = this.getUpNext(row, col);
+            if (nextRowIndex !== -1) {
+                if (this.data[row][col] === 0) {
+                    this.data[row][col] = this.data[nextRowIndex][col];
+                    animation.addTask('' + nextRowIndex + col, '' + row+ col);// 动画
+                    this.data[nextRowIndex][col] = 0;
+                    row--;
+                } else if (this.data[row][col] === this.data[nextRowIndex][col]) {
+                    this.data[row][col] += this.data[nextRowIndex][col];
+                    animation.addTask('' + nextRowIndex + col, '' + row+ col);// 动画
+                    this.data[nextRowIndex][col] = 0;
+                    this.score += this.data[row][col]
+                }
+            } else {
+                break;
+            }
+        }
+    },
+    getUpNext: function (startRow, col) {
+        for (var i = startRow + 1; i < 4; i++) {
+            if (this.data[i][col] !== 0) {
+                return i;
+            }
+        }
+        return -1;
+    },
+
+    /*判断是否可以向某个方向继续移动*/
+    canLeft: function () {
+        for (var row = 0; row < this.data.length; row++) {
+            for (var col = 1; col < this.data[row].length; col++) {
+                if (this.data[row][col] !== 0) {
+                    if (this.data[row][col - 1] === 0 || this.data[row][col] === this.data[row][col - 1]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    },
+    canRight: function () {
+        for (var row = 0; row < this.data.length; row++) {
+            for (var col = 0; col < this.data[row].length - 1; col++) {
+                if (this.data[row][col] !== 0) {
+                    if (this.data[row][col + 1] === 0 || this.data[row][col] === this.data[row][col + 1]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    },
+    canUp: function () {
+        for (var row = 1; row < this.data.length; row++) {
+            for (var col = 0; col < this.data[row].length; col++) {
+                if (this.data[row][col] !== 0) {
+                    if (this.data[row - 1][col] === 0 || this.data[row][col] === this.data[row - 1][col]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    },
+    canDown: function () {
+        for (var row = this.data.length - 1 - 1; row >= 0; row--) {
+            for (var col = 0; col < this.data[row].length; col++) {
+                if (this.data[row][col] !== 0) {
+                    if (this.data[row + 1][col] === 0 || this.data[row + 1][col] === this.data[row][col]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    },
+
+    /*游戏结束*/
+    gameOver: function () {
+        // 如果 满了并且 上下左右不能移动 游戏结束
+        if (this.isFull() && this.canLeft() === false && this.canRight() === false && this.canUp() === false && this.canDown() === false) {
+            return true;
+        }
+    },
+    restart: function () {
+        this.data = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ];
+        this.state = 1;
+        this.score = 0;
+        this.start();
+        document.querySelector('.gameover').style.display = 'none';
+        document.querySelector('#finalScore').innerText = this.score;
+    }
+}
---
+++ animation.js
@@ -0,0 +1,72 @@
+// 构造函数：定义每个对象，每次移动的属性和方法
+function Task(obj, topStep, leftStep) {
+    this.obj = obj;
+    this.topStep = topStep;
+    this.leftStep = leftStep;
+    this.moveStep = function () {
+        var objCSS = null;
+        /* if(this.currentStyle){
+             objCSS = this.obj.currentStyle;
+         }else {
+             objCSS = getComputedStyle(this.obj,null);
+         }*/
+        objCSS = getStyle(this.obj);
+        var top = parseInt(objCSS.top);
+        var left = parseInt(objCSS.left);
+        this.obj.style.top = top + this.topStep + 'px';
+        this.obj.style.left = left + this.leftStep + 'px';
+        this.obj.style.zIndex = 3; // 移动的格子的 zindex
+    },
+        this.clear = function () {
+            this.obj.style.top = '';
+            this.obj.style.left = '';
+            this.obj.style.zIndex = 1; // 还原默认的zindex =1
+        }
+}
+
+var animation = {
+    times: 10, // 移动的次数或步数
+    interval: 16, // 每一秒 迈一步
+    timer: null, // 保存定时器线程号
+    tasks: [],
+    addTask: function (source, target) {
+        var sourceCell = document.querySelector('#fc' + source);
+        var targetCell = document.querySelector('#fc' + target);
+        var sourceCSS = getStyle(sourceCell); // 获取原位置css样式
+        var targetCSS = getStyle(targetCell); // 获取目标位置css样式
+        var topStep = (parseInt(targetCSS.top) - parseInt(sourceCSS.top)) / this.times;
+        var leftStep = (parseInt(targetCSS.left) - parseInt(sourceCSS.left)) / this.times;
+        var task = new Task(sourceCell, topStep, leftStep);
+        this.tasks.push(task);
+    },
+    start: function () { // 启动动画
+        clearInterval(this.timer);
+        this.timer = setInterval(function () {
+            for (var i = 0; i < animation.tasks.length; i++) {
+                var task = animation.tasks[i];
+                task.moveStep();
+            }
+            animation.times--;
+            if (animation.times === 0) {
+                for (var i = 0; i < animation.tasks.length; i++) {
+                    var task = animation.tasks[i];
+                    task.clear();
+                }
+                clearInterval(animation.timer);
+                animation.timer = null;
+                animation.tasks = [];
+                animation.times = 10;
+                //   动画完成
+            }
+        }, this.interval);
+    }
+}
+
+/*获取样式方法 兼容浏览器*/
+function getStyle(obj) {
+    if (this.currentStyle) {
+        return obj.currentStyle;
+    } else {
+        return getComputedStyle(obj, null);
+    }
+}
