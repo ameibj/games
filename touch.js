669fdfe3a676b79351fa7344c3aef233b8ae3fc9
ameibj <576164240@qq.com>
2018-05-04 18:23:44 +0800

    修复苹果高版本页面缩放bug

--- touch.js
+++ touch.js
@@ -31,12 +31,12 @@ function getDirection(startx, starty, endx, endy) {
 }
 
 //手指接触屏幕
-document.addEventListener("touchstart", function (e) {
+/*document.addEventListener("touchstart", function (e) {
     e.stopPropagation();//阻止冒泡
     e.preventDefault();//阻止浏览器默认事件
     startx = e.touches[0].pageX;
     starty = e.touches[0].pageY;
-}, false);
+}, false);*/
 //手指离开屏幕
 /*
 document.addEventListener("touchend", function(e) {
